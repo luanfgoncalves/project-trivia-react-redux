e47327bb9293ba6b0e77e9dcc9e22ab13c50faf1
Peterson <petersonpaganelli@outlook.com>
2022-08-05 16:24:26 -0300

    Requisito 1

---
+++ src/redux/reducers/login.js
@@ -0,0 +1,13 @@
+const INITIAL_STATE = {
+  login: '',
+  email: '',
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default user;
